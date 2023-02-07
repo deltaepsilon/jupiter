f608b4be2f93f37b2591de54452cafd478784ae9
Chris Esplin <chris@chrisesplin.com>
2023-02-07 08:12:33 -0700

    Updating exiftool

--- apps/daemon/src/exif/data.ts
+++ apps/daemon/src/exif/data.ts
@@ -2,6 +2,8 @@ import path from 'path';
 
 const isWindows = process.platform === 'win32';
 
-export const EXIFTOOL_PATH = isWindows ? path.join('vendor', 'exiftool.exe') : path.join('vendor', 'exiftool');
+export const EXIFTOOL_PATH = isWindows
+  ? path.join('vendor', 'exiftool-12.55', 'exiftool.exe')
+  : path.join('vendor', 'Image-ExifTool-12.55', 'exiftool');
 
 export const CONFIG_PATH = path.join(__dirname, 'vendor', 'custom.cfg');
