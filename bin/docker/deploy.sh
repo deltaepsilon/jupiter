b54be9bb4a856733b9cf0465d35d966587342f3a
Chris Esplin <chris@chrisesplin.com>
2023-02-16 12:02:12 -0700

    Attempting to debug and deploy

--- bin/docker/deploy.sh
+++ bin/docker/deploy.sh
@@ -4,7 +4,6 @@ SCRIPT_PATH=$PWD/$(dirname $0)
 REPOSITORY=docker-images
 REGISTRY=us-central1-docker.pkg.dev
 
-# $SCRIPT_PATH/build.sh
 docker build . -t $DEPLOY_NAME:latest
 PROJECT_ID=$PROJECT_ID $SCRIPT_PATH/../gcloud/configure.sh
 
@@ -12,8 +11,6 @@ gcloud config set run/region us-central1
 gcloud config set account chris@christopheresplin.com
 gcloud auth configure-docker us-central1-docker.pkg.dev
 
-# gcloud builds submit --tag gcr.io/$PROJECT_ID/$DEPLOY_NAME
-# gcloud builds list
 docker tag $DEPLOY_NAME:latest $REGISTRY/$PROJECT_ID/$REPOSITORY/$DEPLOY_NAME
 docker push $REGISTRY/$PROJECT_ID/$REPOSITORY/$DEPLOY_NAME
 gcloud run deploy $DEPLOY_NAME --allow-unauthenticated --port=3000 --image $REGISTRY/$PROJECT_ID/$REPOSITORY/$DEPLOY_NAME
