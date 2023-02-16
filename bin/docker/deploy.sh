f54b88fb2dcb7b7aba0ca7a01c184be0c7110582
Chris Esplin <chris@chrisesplin.com>
2023-02-16 15:47:34 -0700

    Repair attempt

--- bin/docker/deploy.sh
+++ bin/docker/deploy.sh
@@ -1,17 +1,17 @@
 PROJECT_ID=photos-tools-2022
 DEPLOY_NAME=jupiter-web
+REGION=us-central1
 SCRIPT_PATH=$PWD/$(dirname $0)
 REPOSITORY=docker-images
-REGISTRY=us-central1-docker.pkg.dev
+REGISTRY=$REGION-docker.pkg.dev
 
 docker build . -t $DEPLOY_NAME:latest
 
 # PROJECT_ID=$PROJECT_ID $SCRIPT_PATH/../gcloud/configure.sh
 # gcloud config set account chris@christopheresplin.com
 
-gcloud config set run/region us-central1
-gcloud config configurations list
-gcloud auth configure-docker us-central1-docker.pkg.dev
+gcloud config set run/region $REGION
+echo y | gcloud auth configure-docker $REGISTRY
 
 docker tag $DEPLOY_NAME:latest $REGISTRY/$PROJECT_ID/$REPOSITORY/$DEPLOY_NAME
 docker push $REGISTRY/$PROJECT_ID/$REPOSITORY/$DEPLOY_NAME
