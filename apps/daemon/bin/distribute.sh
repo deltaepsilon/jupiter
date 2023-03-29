bbab52804e00bba21163a1c06add66a25be67172
Chris Esplin <chris@chrisesplin.com>
2023-03-29 08:58:16 -0600

    Attempting to sign windows binaries

--- apps/daemon/bin/distribute.sh
+++ apps/daemon/bin/distribute.sh
@@ -4,6 +4,7 @@ DIST_PATH=$DAEMON_PATH/dist # /root/dev/jupiter/apps/daemon/dist
 VENDOR_PATH=$DAEMON_PATH/vendor # /root/dev/jupiter/apps/daemon/vendor
 VENDOR_CONFIG_PATH=$DAEMON_PATH/vendor/custom.cfg # /root/dev/jupiter/apps/daemon/vendor
 WEB_DAEMON_PATH=$DAEMON_PATH/../web/public/daemon # /root/dev/jupiter/apps/web/daemon
+SIGN_PATH=$SCRIPT_PATH/sign # /root/dev/jupiter/apps/daemon/bin/sign
 
 buildDaemon() {
   DAEMON_DIRECTORY=$DIST_PATH/quiver-photos-$1 # /root/dev/jupiter/apps/daemon/dist/linux
@@ -31,6 +32,20 @@ buildDaemon "linux-arm64" "daemon-linux-arm64" "Image-ExifTool-12.55"
 buildDaemon "macos-x64" "daemon-macos-x64" "Image-ExifTool-12.55"
 buildDaemon "windows-x64" "daemon-win-x64.exe" "exiftool-12.55"
 buildDaemon "windows-arm64" "daemon-win-arm64.exe" "exiftool-12.55"
+
+signWindows() {
+  rm $DIST_PATH/$1/$2-signed.exe || true
+  
+  $SIGN_PATH/osslsigncode sign -certs $SIGN_PATH/certs/bundle.crt -key $SIGN_PATH/certs/codesign.key -h sha256 -n "Chris Esplin" -i "https://photos.chrisesplin.com" -t "http://timestamp.sectigo.com" -in  $DIST_PATH/$1/$2.exe -out $DIST_PATH/$1/$2-signed.exe
+
+  mv $DIST_PATH/$1/$2.exe $DIST_PATH/$1/$2-unsigned.exe
+  mv $DIST_PATH/$1/$2-signed.exe $DIST_PATH/$1/$2.exe
+  rm $DIST_PATH/$1/$2-unsigned.exe
+}
+
+signWindows quiver-photos-windows-x64 daemon-win-x64
+signWindows quiver-photos-windows-arm64 daemon-win-arm64
+
 
 echo "Zipping folders..."
 zipFolder() {
