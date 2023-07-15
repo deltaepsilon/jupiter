065de7f8c7aaee64fd2c7455e9f28d3f591a309e
Chris Esplin <chris@chrisesplin.com>
2023-07-15 13:36:17 -0600

    Exponential backoff for refresh-media-items

--- apps/daemon/src/download/refresh-media-items.ts
+++ apps/daemon/src/download/refresh-media-items.ts
@@ -1,7 +1,10 @@
+import axios, { AxiosResponse } from 'axios';
+
 import { LevelDatabase } from '../level';
-import axios from 'axios';
 import { batchGetMediaItemsResponseSchema } from 'data/media-items';
 import { createGettersAndSetters } from '../utils';
+
+const MAX_TRIES = 20;
 
 export async function refreshMediaItems({
   db,
@@ -15,12 +18,33 @@ export async function refreshMediaItems({
   const { getTokens, getUrls, setTokens, setMediaItem } = createGettersAndSetters(db);
   const tokens = await getTokens();
   const urls = await getUrls();
+  let tries = 0;
+  let result: AxiosResponse<any, any> | null = null;
 
-  const result = await axios.post(urls.batchGetMediaItems, {
-    accessToken: tokens.accessToken,
-    refreshToken: tokens.refreshToken,
-    mediaItemIds: mediaItemIds.join(','),
-  });
+  while (tries < MAX_TRIES) {
+    result = await axios
+      .post(urls.batchGetMediaItems, {
+        accessToken: tokens.accessToken,
+        refreshToken: tokens.refreshToken,
+        mediaItemIds: mediaItemIds.join(','),
+      })
+      .catch(() => null);
+
+    if (result) {
+      break;
+    } else {
+      tries++;
+
+      await wait(Math.pow(2, tries) * 3000);
+    }
+  }
+
+  if (!result) {
+    throw new Error(`Failed to refresh media items: ${mediaItemIds.length} count`);
+  } else {
+    console.info(`[refreshMediaItems] success: ${mediaItemIds.length} count`);
+  }
+
   const { accessToken, refreshToken, expiresAt, invalidMediaIds, mediaItemResults } =
     batchGetMediaItemsResponseSchema.parse(result.data);
 
@@ -36,4 +60,8 @@ export async function refreshMediaItems({
       })
     ),
   };
+}
+
+async function wait(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
