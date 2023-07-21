15585ef8dd02bc46245432e9e47fa47e9a7928f9
Chris Esplin <chris@chrisesplin.com>
2023-07-21 11:21:21 -0600

    Manually set custom claims

---
+++ bin/set-custom-claim.js
@@ -0,0 +1,21 @@
+const { initializeApp, applicationDefault } = require('firebase-admin/app');
+const { getAuth } = require('firebase-admin/auth');
+const path = require('path');
+const credentialPath = path.join(__dirname, '../apps/firebase/service-account.json');
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialPath;
+
+const app = initializeApp({
+  credential: applicationDefault(),
+  projectId: 'photos-tools-2022',
+});
+
+const auth = getAuth(app);
+
+(async () => {
+  const userId = 'NHrzIfJBSqRoNTCQLMb0kc0vOrj2';
+  await auth.setCustomUserClaims(userId, { stripeRole: 'subscriber' });
+  const userRecord = await auth.getUser(userId);
+
+  console.log('userRecord', userRecord.customClaims);
+})();
